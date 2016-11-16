6818222dee89542e9f57fc15ac9751d8e27c8334
houxinjie <houxinjie@gmail.com>
2016-11-16 11:32:38 +0800

    调成程序为命令行模式，增加README.md文件

---
+++ README.md
@@ -0,0 +1,23 @@
+# flv_to_mp4
+
+a tool can remux flv to mp4.
+
+### Installation
+
+Prerequisites: [Node.js](https://nodejs.org/en/) (>=7.x) and [Git](https://git-scm.com/).
+
+``` bash
+$ npm install -g https://github.com/houxinjie/flv_to_mp4
+```
+
+### Usage
+
+``` bash
+$ ftom4 -i <inputFile> -o <outFile>
+```
+
+Example:
+
+``` bash
+$ ftom4 -i data/test.flv -o data/test.mp4
+```
--- index.js
+++ index.js
@@ -1,7 +1,59 @@
-require("babel-core/register")(
-    {
-        presets: ['es2015']
-    }
-);
+import path from 'path';
+import fs from 'fs';
+import FLVDemuxer from './demux/flv-demuxer.js';
+import MP4Remuxer from './remux/mp4-remuxer.js';
+import Log from './utils/logger.js';
+import commander from 'commander';
+import {version} from './package.json';
 
-require("./app.js");
+
+commander
+    .version(version)
+    .option('-i, --input <inputFile>')
+    .option('-o, --output <outputFile>')
+    .usage('-i <inputFile> -o <outputFile>')
+    .parse(process.argv)
+
+const Tag = 'Ftom4'
+
+if(!commander.input || !commander.output) {
+    commander.help()
+    process.exit(0);
+}
+
+const inputFilePath = path.join(process.cwd(), commander.input);
+const outputFilePath = path.join(process.cwd(), commander.output);
+
+if(!fs.existsSync(inputFilePath)){
+    Log.e(Tag, 'input file not exist');
+    process.exit(0);
+}
+
+if(fs.existsSync(outputFilePath)){
+    fs.unlinkSync(outputFilePath);
+}
+
+const arrayBuffer = fs.readFileSync(inputFilePath).buffer;
+
+const demuxer = new FLVDemuxer(arrayBuffer);
+const remuxer = new MP4Remuxer();
+
+demuxer.onError = () => {
+
+}
+
+demuxer.onMediaInfo = () => {
+
+}
+
+remuxer.bindDataSource(demuxer);
+
+remuxer.onInitSegment = data => {
+    fs.writeFileSync(outputFilePath, Buffer.from(data), {flag: 'a'})
+}
+
+remuxer.onMediaSegment = (type, mediaSegment) => {
+    fs.writeFileSync(outputFilePath, Buffer.from(mediaSegment.data), {flag: 'a'})
+}
+
+demuxer.parseChunks(arrayBuffer, 0);
